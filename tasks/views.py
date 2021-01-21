f039f1c559346afce86b9194d364e76905f334f5
Laurence Zanotti <lau_zanotti@hotmail.com>
2021-01-21 17:33:21 -0300

    Added templates and layout.html

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+tasks = ['Lavar a louça', 'Tirar roupa do varal', 'Jogar videogame']
+
 # Create your views here.
 def index(request):
-    return render(request, "tasks/index.html")
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+    })
+
+
+def add(request):
+    return render(request, "tasks/add.html")
