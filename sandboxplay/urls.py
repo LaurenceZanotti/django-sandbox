fe6f75d0a5d25eea1180ee2ed9a6711a9b9d5a09
Laurence Zanotti <lau_zanotti@hotmail.com>
2021-01-21 17:05:08 -0300

    Tasks app initial setup

--- sandboxplay/urls.py
+++ sandboxplay/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', include("hello.urls")),
-    path('celebration/', include("celebration.urls"))
+    path('celebration/', include("celebration.urls")),
+    path('tasks/', include("tasks.urls"))
 ]
