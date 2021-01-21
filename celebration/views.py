25cfb718867205572e308e902ce3269dcc412cc8
Laurence Zanotti <lau_zanotti@hotmail.com>
2021-01-21 15:19:00 -0300

    Celebration app finished

--- celebration/views.py
+++ celebration/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from pyluach import dates
 
 # Create your views here.
 def index(request):
-    return render(request, "celebration/index.html")
+    # Jesus Christ Celebration at day 14th, Nissan (when he died)
+    celebrationDate = dates.HebrewDate(
+        dates.HebrewDate.today().year,
+        1,
+        14
+    )
+
+    # Check if current date is celebration date
+    isCeleb = None
+    if celebrationDate.to_jd == dates.GregorianDate.today().to_jd:
+        isCeleb = True
+    else:
+        isCeleb = False
+
+    return render(request, "celebration/index.html", {
+        "date": celebrationDate.to_greg,
+        "isCeleb": isCeleb
+    })
