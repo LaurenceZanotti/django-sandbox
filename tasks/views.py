2bcfa450bc949313387c7fb73fb886bd351a8b7d
Laurence Zanotti <lau_zanotti@hotmail.com>
2021-01-22 16:42:45 -0300

    Added refined comments about what I've learned. Next: Do server-side validation

--- tasks/views.py
+++ tasks/views.py
@@ -4,12 +4,22 @@ from django import forms
 tasks = []
 
 class NewTaskForm(forms.Form):
-    # Labels are put on by default and named after the variable (in this case, new_task will be parsed as "New task:", if set to True)
-    # To specify attributes, we need to add a widget as below (the details of why it is implemented this way is yet unknown to me).
+    """
+    • Labels are put on by default and named after the variable name (in this case, new_task will be parsed as "New task:", if set to True)
+
+    • To specify attributes, we need to add a widget as below (the details of why it is implemented this way is yet unknown to me).
+
+    """
     new_task = forms.CharField(label=False, widget=forms.TextInput(attrs={"class": "form-control mt-1", "style": "width: 35%"}), required=True)
-    # In order to use min_value and max_value (and potentially other tools for validation) while using a widget (like 
-    # in this case, for CSS class), you must match the type of the widget and the type of field (e.g. IntegerField/NumberInput,
-    # and in other cases, probably CharField/TextInput). min and max_value didn't work when those weren't matched.
+    """
+    • In order to use min_value and max_value (and potentially other tools for validation) while using a widget (like 
+    in this case, for CSS class), you must match the type of the widget and the type of field (e.g. IntegerField/NumberInput,
+    and in other cases, probably CharField/TextInput). min and max_value didn't work when those weren't matched.
+
+    • Multiple attrs (attributes) can be placed in dictionary format. In the first field I decided to use inline-css, while the second
+    field I opted to use the class "input-width", which was written in the style tag in the template file.
+
+    """
     priority = forms.IntegerField(min_value=0, max_value=10, widget=forms.NumberInput(attrs={"class": "form-control input-width mt-1"}))
 
 
